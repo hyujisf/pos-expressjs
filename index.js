8c9cbbcff46562019115db64d30d17a240b5784b
hyujisf <canqalloid.me@gmail.com>
2023-05-06 11:59:02 +0700

    fixing POST Product

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const db = require('./config/connection')
 const port = 3000
 
+// db.connect()
 
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.send("testing");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
